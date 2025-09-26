900045836a11abc50fedeae545bb33639c2452ca
lobna mohamed <60537323+Lobnaal124@users.noreply.github.com>
2025-09-26 18:07:38 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -7,142 +7,158 @@
 
 #include "main.h"
 
-//Arabic name 
-void LCD_CreateChar(u8 location, u8 charmap[]) {
-	location &= 0x07;
-	LCD_voidSendCommand(0x40 | (location << 3)); // location on CGRAM
-	for (u8 i = 0; i < 8; i++) {
-		LCD_voidSendData(charmap[i]); //show data on LCD
+
+u8 get_key(){
+	
+	u8 key=0xff;
+		do 
+		{
+			key=KEYPADu8GetKey();
+			
+		} while (key==0xff);
+
+	return key;
+}
+
+volatile static u16 tick = 0;
+volatile static u8 sec = 0;
+volatile static u8 min = 0;
+volatile static u8 hour = 0;
+
+void APP(void)
+{  tick++;
+
+	if(tick >= 1000)  
+	{
+		tick = 0;
+		sec++;
+
+		if(sec == 60)
+		{
+			sec = 0;
+			min++;
+			LED_On(DIO_PORTB,DIO_PIN3);
+		}
+
+		if(min == 60)
+		{
+			min = 0;
+			hour++;
+			LED_On(DIO_PORTC,DIO_PIN2);
+			
+		}
+
+		if(hour == 1)  
+		{ 
+			hour = 0;
+			min = 0;
+			sec = 0;
+			LED_On(DIO_PORTC,DIO_PIN7);
+			
+		}
+		LCD_voidGoToxy(0,0);
+
+		if(hour < 10)
+	    LCD_voidSendData('0');
+		LCD_voidSendNumber(hour);
+		LCD_voidSendData(':');
+
+		if(min < 10)
+	    LCD_voidSendData('0');
+		LCD_voidSendNumber(min);
+		LCD_voidSendData(':');
+
+		if(sec < 10)
+	    LCD_voidSendData('0');
+		LCD_voidSendNumber(sec);
 	}
 }
-int main(void)
-{   
+
+/*
+	//LED_init
 	DIO_voidSetPortDir(LCD_DPORT,0xf0);
-	DIO_voidSetPortDir(LCD_CPORT,0x07);
+	DIO_voidSetPortDir(LCD_CPORT,0xfe);
 	LCD_voidInit();
-	
-	
-	// name in Arabic 
-      u8 arr[]= {
-	    0x01,
-	    0x01,
-	    0x01,
-	    0x05,
-	    0x05,
-	    0x1F,
-	    0x08,
-	    0x00};
-	 u8 arr_2[]={
-	    0x00,
-	    0x00,
-	    0x00,
-	    0x0A,
-	    0x02,
-	    0x1F,
-	    0x00,
-	    0x00};
-	 u8 arr_3[]={
-	    0x00,
-	    0x00,
-	    0x00,
-	    0x0E,
-	    0x1A,
-	    0x1B,
-	    0x18,
-	    0x00};
-		LCD_CreateChar(0,arr);
-		LCD_CreateChar(1,arr_2);
-		LCD_CreateChar(2,arr_3);
-		
-		LCD_voidGoToxy(15,0);
-		LCD_voidSendData(0);
-		LCD_voidGoToxy(14,0);
-		LCD_voidSendData(1);
-		LCD_voidGoToxy(13,0);
-		LCD_voidSendData(2);
-		_delay_ms(500);
-		LCD_voidSendCommand(1);
-		
-		LCD_voidGoToxy(8,1);
-		LCD_voidSendData(0);
-		LCD_voidGoToxy(7,1);
-		LCD_voidSendData(1);
-		LCD_voidGoToxy(6,1);
-		LCD_voidSendData(2);
-		_delay_ms(500);
-		
-		LCD_voidSendCommand(1);
-		LCD_voidGoToxy(2,0);
-		LCD_voidSendData(0);
-		LCD_voidGoToxy(1,0);
-		LCD_voidSendData(1);
-		LCD_voidGoToxy(0,0);
-		LCD_voidSendData(2);
-		
-	 /*	
-	// khatem el_tasbih	
-	DIO_voidSetPinDir(DIO_PORTC,DIO_PIN0,INPUT);//count+
-	DIO_voidSetPinDir(DIO_PORTC,DIO_PIN1,INPUT);//count-
-	DIO_voidSetPinDir(DIO_PORTC,DIO_PIN2,INPUT);//Reset
-	DIO_voidSetPinVal(DIO_PORTC,DIO_PIN0,HIGH);
-	DIO_voidSetPinVal(DIO_PORTC,DIO_PIN1,HIGH);
-	DIO_voidSetPinVal(DIO_PORTC,DIO_PIN2,HIGH);
-	u8 count=0;  */
-  
-  /*
-  //Hockey Game 
-   u8 Ball[]   = {0x00,0x00,0x0E,0x0E,0x0E,0x00,0x00,0x00};
-   u8 Paddle[] = {0x1F,0x1F,0x1F,0x1F,0x1F,0x1F,0x1F,0x1F};
+*/
 
-   DIO_voidSetPinDir(DIO_PORTC,DIO_PIN0,INPUT);//up
-   DIO_voidSetPinDir(DIO_PORTC,DIO_PIN1,INPUT);//down
-   DIO_voidSetPinDir(DIO_PORTC,DIO_PIN2,INPUT);//reset
-   DIO_voidSetPinVal(DIO_PORTC,DIO_PIN0,HIGH);
-   DIO_voidSetPinVal(DIO_PORTC,DIO_PIN1,HIGH);
-   DIO_voidSetPinVal(DIO_PORTC,DIO_PIN2,HIGH); */
 
-  
-  
-    while (1) 
-    {
-		
-		
-		
-		 /* 
-		// khatem el_tasbih
-		    if (DIO_u8ReadPinVal(DIO_PORTC,DIO_PIN0)==0)
-		    {
-			    while(DIO_u8ReadPinVal(DIO_PORTC,DIO_PIN0)==0);
-			    _delay_ms(5);
-			    LCD_voidGoToxy(0,0);
-			    LCD_voidSendString("count:");
-				count++;
-			    LCD_voidGoToxy(6,0);
-			    LCD_voidSendNumber(count);
-				
-		    }
-		else if (DIO_u8ReadPinVal(DIO_PORTC,DIO_PIN1)==0)
-		{
-			while(DIO_u8ReadPinVal(DIO_PORTC,DIO_PIN1)==0);
-			_delay_ms(5);
-			LCD_voidGoToxy(0,0);
-			LCD_voidSendString("count:");
-			LCD_voidGoToxy(6,0);
-			LCD_voidSendNumber(count);
-			count--;
-		}
-	else if (DIO_u8ReadPinVal(DIO_PORTC,DIO_PIN2)==0)
-		{
-			while(DIO_u8ReadPinVal(DIO_PORTC,DIO_PIN2)==0);
-			_delay_ms(5);
-			LCD_voidGoToxy(0,0);
-			LCD_voidSendString("reset:");
-			LCD_voidSendNumber(0);
-		}*/
-      
-	  
-	  //Hockey Game 
-        
+int main(void)
+{  
+	GI_voidEnable();
+	TIM0_voidInit(CTC_MODE);
+    TIM_0_Callback(APP,CTC_MODE);
+	
+	DIO_voidSetPortDir(LCD_DPORT,0xf0);
+	DIO_voidSetPortDir(LCD_CPORT,0xfe);
+	LCD_voidInit();
+   // KEYPAD_voidInit();
+	//LCD_voidGoToxy(0,0);
+	//LCD_voidSendString("ENTER PASS:");
+   //u8 key=0;
+   //u8 i=0;
+   //u8 pass[4]={1,2,3,4};
+   //u8 entered[4];
+   //u8 check;
+   //u8 attempts=0;
+    while (1)
+    {/*
+	 check=1;	
+	for(i=0;i<4;i++)
+	{  
+	key=get_key();
+	entered[i]=key;
+	LCD_voidGoToxy(12+i,0);
+	LCD_voidSendNumber(entered[i]);
+	_delay_ms(300);
+	LCD_voidGoToxy(12+i,0);
+	LCD_voidSendData('*');
 	}
-   }
+
+	for ( i=0;i<4;i++)
+	{
+		if (entered[i]!=pass[i])
+		{
+			check=0;
+			break;
+		}
+	}
+		if (check==1)
+		{
+			LCD_voidSendCommand(1);
+			LCD_voidGoToxy(0,0);
+			LCD_voidSendString("System OPen!");
+			LED_On(DIO_PORTD,DIO_PIN0);
+			_delay_ms(300);
+			LCD_voidSendCommand(1);
+			LCD_voidGoToxy(0,0);
+			LCD_voidSendString("Welcome Lobna");
+		}
+		 else
+		 {
+			 attempts++;
+			 if (attempts >= 3)
+			 {
+				 LCD_voidSendCommand(1);
+				 LCD_voidGoToxy(0,0);
+				 LCD_voidSendString("System Locked!");
+				 LED_On(DIO_PORTD,DIO_PIN1); 
+				 break;
+			 }
+			 else
+			 {   
+				 LCD_voidSendCommand(1);
+				 LCD_voidGoToxy(0,0);
+				 LCD_voidSendString("Try Again!"); 
+				 LED_On(DIO_PORTD,DIO_PIN1);
+				 _delay_ms(500);
+				 LCD_voidSendCommand(1);
+				 LCD_voidGoToxy(0,0);
+				 LCD_voidSendString("ENTER PASS:");
+				 
+			 }
+		 }*/
+	
+	}
+}
+   
 
